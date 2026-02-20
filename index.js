46bcb20c1b363ba94005435df6a1b64b007b871e
angel <mather@.com>
2026-02-20 14:49:55 -0300

    error 21/02 1

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.post("/send", async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, () => {
     console.log("API en puerto", PORT);
 });
